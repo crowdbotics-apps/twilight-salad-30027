08eb20459849bf93f7d4a1babf94993ecd90f32a
Crowdbotics <team@crowdbotics.com>
2021-08-23 01:04:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "twilight_salad_30027Identifier",
+  url: "https://twilight_salad_30027Identifier.botics.co",
+  api: "https://twilight_salad_30027Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
